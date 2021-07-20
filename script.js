640eacf19a705b0f80a5de1d7827cb8a0df9bc37
vaibhav1236 <87272843+vaibhav1236@users.noreply.github.com>
2021-07-20 07:38:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+var name = prompt("enter the name of farmer ");
+document.write("name of the farmer is:", name, "<br>")
+
+var R=prompt("enter the rate of sugarcane")
+var r1=parseInt(R)
+
+var weight1 = prompt("enter the weight of day 1")
+document.write("weight of sugarcane of day 1 is :", weight1, "<br>")
+var num1 =parseInt(weight1)
+
+var weight2 = prompt("enter the weight of day 2")
+document.write("weight of sugarcane of day 2 is :", weight2, "<br>")
+var num2 =parseInt(weight2)
+
+var weight3 = prompt("enter the weight of day 3")
+document.write("weight of sugarcane of day 3 is :", weight3, "<br>")
+var num3 =parseInt(weight3)
+
+
+var weight4 = prompt("enter the weight of day 4")
+document.write("weight of sugarcane of day 4 is :", weight4, "<br>")
+var num4 =parseInt(weight4)
+
+
+var weight5 = prompt("enter the weight of day 5")
+document.write("weight of sugarcane of day 5 is :", weight5, "<br>")
+var num5 =parseInt(weight5)
+
+
+var weight6 = prompt("enter the weight of day 6")
+document.write("weight of sugarcane of day 6 is :", weight6, "<br>")
+var num6 =parseInt(weight6)
+
+
+var weight7 = prompt("enter the weight of day 7")
+document.write("weight of sugarcane of day 7 is :", weight7, "<br>")
+var num7 =parseInt(weight7)
+
+
+var weight8 = prompt("enter the weight of day 8")
+document.write("weight of sugarcane of day 8 is :", weight8, "<br>")
+var num8 =parseInt(weight8)
+
+
+var weight9 = prompt("enter the weight of day 9","<br>")
+document.write("weight of sugarcane of day 9 is :", weight9, "<br>")
+var num9 =parseInt(weight9)
+
+
+var weight10 = prompt("enter the weight of day 10")
+document.write("weight of sugarcane of day 10 is :", weight10, "<br>")
+var num10 =parseInt(weight10)
+
+document.write("Total weight of sugarcane is =",(num1 + num2 + num3+num4+num5+num6+num7+num8+num9+num10), "<br>");
+
+document.write("total payment  of farmer is :", (num1 + num2 + num3+num4+num5+num6+num7+num8+num9+num10)*r1)
